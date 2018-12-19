47fe35a3630c6de3cf6151839761266f4ed1d9ee
Gyrych <gyrych@qq.com>
2018-12-19 20:04:07 +0800

    初次提交代码

---
+++ User/main.c
@@ -0,0 +1,17 @@
+#include <wiringPi.h>
+
+int main()
+{
+	wiringPiSetup();
+	pinMode(1, OUTPUT);
+
+	while(1)
+	{
+		digitalWrite(1, HIGH);
+		delay(500);
+		digitalWrite(1, LOW);
+		delay(500);
+	}
+
+	return 0;
+}
